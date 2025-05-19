5befe513ec9184e455960ab84ce358dc01807da5
Frederic Lamagdelaine <f.lamag@free.fr>
2025-05-19 15:25:32 +0200

    exo2 v2

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -19,6 +19,42 @@
 # end
 
 # test/system/questions_test.rb
+# require "application_system_test_case"
+
+# class QuestionsTest < ApplicationSystemTestCase
+#   # [...]
+
+#   test "saying Hello yields a grumpy response from the coach" do
+#     visit ask_url
+#     fill_in "question", with: "Hello"
+#     click_on "Ask"
+
+#     take_screenshot  # Prend une capture d'écran ici
+
+#     assert_text "I don't care, get dressed and go to work!"
+#   end
+
+#   test "On peut manger ?" do
+#     visit ask_url
+#     fill_in "question", with: "On peut manger ?"
+#     click_on "Ask"
+
+#     take_screenshot  # Prend une capture d'écran ici
+
+#     assert_text "Silly question, get dressed and go to work!"
+#   end
+
+#   test "I am going to work" do
+#     visit ask_url
+#     fill_in "question", with: "I am going to work"
+#     click_on "Ask"
+
+#     take_screenshot  # Prend une capture d'écran ici
+
+#     assert_text "Great!"
+#   end
+# end
+
 require "application_system_test_case"
 
 class QuestionsTest < ApplicationSystemTestCase
@@ -28,27 +64,27 @@ class QuestionsTest < ApplicationSystemTestCase
     visit ask_url
     fill_in "question", with: "Hello"
     click_on "Ask"
-
+    sleep(1)
     take_screenshot  # Prend une capture d'écran ici
 
     assert_text "I don't care, get dressed and go to work!"
   end
 
-  test "On peut manger ?" do
+   test "avec question" do
     visit ask_url
-    fill_in "question", with: "On peut manger ?"
+    fill_in "question", with: "tralalal ?"
     click_on "Ask"
-
+    sleep(1)
     take_screenshot  # Prend une capture d'écran ici
-
+    
     assert_text "Silly question, get dressed and go to work!"
   end
 
-  test "I am going to work" do
+   test "autre" do
     visit ask_url
     fill_in "question", with: "I am going to work"
     click_on "Ask"
-
+    sleep(1)
     take_screenshot  # Prend une capture d'écran ici
 
     assert_text "Great!"
